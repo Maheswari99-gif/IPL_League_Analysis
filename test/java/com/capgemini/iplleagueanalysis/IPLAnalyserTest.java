88145a2fcc7bd7066cbeebcc25a484ec01d810bc
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-29 01:03:09 +0530

    MaheswariKoyyani : Top batting averages of cricketers

---
+++ test/java/com/capgemini/iplleagueanalysis/IPLAnalyserTest.java
@@ -0,0 +1,30 @@
+package com.capgemini.iplleagueanalysis;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IPLAnalyserTest {
+
+	private final String PLAYER_RUNS_DATA = "C:\\Users\\MAHI\\eclipse-workspace\\iplLeagueAnalysis\\src\\main\\resources\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+
+	IPLAnalyser iplAnalyser = null;
+
+	@Before
+	public void createIplAnalyserObject() {
+		iplAnalyser = new IPLAnalyser();
+	}
+
+	@Test
+	public void givenCsvDataShouldReturnTopBattingAvg() {
+		try {
+			iplAnalyser.loadRunsData(PLAYER_RUNS_DATA);
+			String playerName = iplAnalyser.getTopBattingAvg();
+			assertEquals("MS Dhoni", playerName);
+		} catch (IPLAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
